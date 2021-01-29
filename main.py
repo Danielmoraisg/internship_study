a33a8ce4703fc638555c88f058cb8d70994da2a2
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2021-01-29 12:20:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from PIL import Image
 img = Image.open('pharmacy.png').convert('RGB')
 #paginas
 import home
-import test
 
 st.set_page_config(page_title="Interntrial",page_icon=img,layout="centered",initial_sidebar_state="auto")
 state = _get_state()
 
 PAGES = {
-	"Settings":home,
-	"Test":test
+	"Settings":home
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
