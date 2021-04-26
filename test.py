28f1dd8f25e8406bd9570112bc54dfdaad9bafac
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2021-04-26 09:32:16 -0300

    Add files via upload
    added new features to the test and home pages

--- test.py
+++ test.py
@@ -8,10 +8,16 @@ def app(state):
 	data = r.content
 	df = pd.read_html(data, skiprows = 1)[0].iloc[:,1:].dropna(how = 'all', axis = 1).dropna(how = 'all', axis = 0)
 	df['Categories'] = list(map(lambda x : x.split(','),df.Categories))
+	df['sub categories'] = list(map(lambda x : x.split(','),df['sub categories']))
 	if state.random:
 		df = df.explode('Categories')
-		category = st.selectbox('Select the category you want to train',sorted(list(set(df.Categories))))
-		subdf = df[df['Categories'] == category]
+		if state.subcategories:
+			df = df.explode('sub categories')
+			subcategory = st.selectbox('Select the subcategory you want to train',sorted(list(set(df['sub categories']))))
+			subdf = df[df['sub categories'] == subcategory]
+		else:
+			category = st.selectbox('Select the category you want to train',sorted(list(set(df.Categories))))
+			subdf = df[df['Categories'] == category]
 		if st.button('Next question'):
 			samp = subdf.sample()
 			q = samp['Question'].values[0]
@@ -31,8 +37,12 @@ def app(state):
 </body>
 </html>"""
 )
-			with st.beta_expander('See answer'):
+			with st.beta_expander('See hint'):
+				st.write(samp['hint'].values[0])
+			with st.beta_expander('See answer & Categories'):
 				st.write(samp['Answer'].values[0])
+				st.write("Categories: " + ", ".join(samp.Categories.values[0]))
+				st.write("Subcategories: " + ", ".join(samp['sub categories'].values[0]))
 		else:
 			st.write('Click the button to get a question')
 	else:
@@ -55,8 +65,11 @@ def app(state):
 </body>
 </html>"""
 				)
+			with st.beta_expander('See hint'):
+				st.write(samp['hint'].values[0])
 			with st.beta_expander('See answer & Categories'):
 				st.write(samp['Answer'].values[0])
 				st.write("Categories: " + ", ".join(samp.Categories.values[0]))
+				st.write("Subcategories: " + ", ".join(samp['sub categories'].values[0]))
 		else:
 			st.write('Click the button to get a question')
